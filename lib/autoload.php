d379b709b1a1eca31db501a5339a6f6c628f195d
Steven De Ryck <steven@inform.be>
2019-12-04 23:14:34 +0100

    Aanpassingen passwd

--- lib/autoload.php
+++ lib/autoload.php
@@ -3,7 +3,8 @@ session_start();
 $_SESSION["head_printed"] = false;
 $_application_folder = "/wdev_steven/testremote";
 
-require_once "pdo.php";                         //database functies
+require_once "passwd.php";
+require_once "pdo.php";                          //database functies
 require_once "view_functions.php";      //basic_head, load_template, replacecontent...
 require_once "authorisation.php";      //controle login e.d.
 require_once "show_messages.php";
--- lib/pdo.php
+++ lib/pdo.php
@@ -1,43 +1,17 @@
 <?php
-function ReadPasswd()
-{
-    global $_application_folder;
-    global $dbhost, $dbname, $dbuser, $dbpasswd;
-
-    $contents = file_get_contents($_SERVER['DOCUMENT_ROOT'] . $_application_folder . "/lib/passwd.php");
-    $lines = explode("\n", $contents);
-
-    foreach( $lines as $line)
-    {
-        $line = str_replace("\r", "", $line);               // \r verwijderen uit lijn
-        list( $key, $value ) = explode("=", $line);
-        $key = trim($key);                                        //spaties verwijderen uit key
-        $value = trim($value);                                 //spaties verwijderen uit value
-
-        if ( $key == "dbhost" ) $dbhost = $value;
-        if ( $key == "dbname" ) $dbname = $value;
-        if ( $key == "dbuser" ) $dbuser = $value;
-        if ( $key == "dbpasswd" ) $dbpasswd = $value;
-    }
-
-}
-
 function GetConnection()
 {
-    global $dbhost, $dbname, $dbuser, $dbpasswd;
-    /*
-    $dsn = "mysql:host=localhost;dbname=steden";
-    $user = "root";
-    $passwd = "steven123";
-    */
+    require_once "passwd.php";
+    $arr_connection = GetConnectionData();
 
-    ReadPasswd();
+    $dbhost = $arr_connection['dbhost'];
+    $dbname = $arr_connection['dbname'];
+    $dbuser = $arr_connection['dbuser'];
+    $dbpasswd = $arr_connection['dbpasswd'];
 
     $dsn = "mysql:host=$dbhost;dbname=$dbname" ;
-    $user = $dbuser ;
-    $passwd = $dbpasswd ;
 
-    $pdo = new PDO($dsn, $user, $passwd);
+    $pdo = new PDO($dsn, $dbuser, $dbpasswd);
     return $pdo;
 }
 
